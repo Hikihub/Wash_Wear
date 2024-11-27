519246363b13763027d99746ab1ceda58503181c
Hikihub <vikswissa@gmail.com>
2024-11-27 11:05:42 +0000

    Setting up projects

--- app/src/main/java/com/example/wash_wear/auth/SignupActivity.java
+++ app/src/main/java/com/example/wash_wear/auth/SignupActivity.java
@@ -3,15 +3,7 @@ package com.example.wash_wear.auth;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.example.wash_wear.R;
 import com.example.wash_wear.databinding.SignupScreenBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
@@ -42,10 +34,6 @@ public class SignupActivity extends AppCompatActivity {
 
         //initializing progress dialog
         loadingBar = new ProgressDialog(this);
-
-        //Register button click listener
-
-
 
 
 
